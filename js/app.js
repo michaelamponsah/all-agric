fc6d98346165bd0e5b79a9809126252cbd6311c1
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-10-21 20:01:55 +0000

    ...#

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("click", (e) => {
+  const isSliderNav = e.target.matches("[data-slider-button]");
+  if (!isSliderNav && e.target.closest("[data-slider]") != null) return;
+
+  let currentSliderNav;
+  if (isSliderNav) {
+    currentSliderNav = e.target.closest("[data-slider]");
+    currentSliderNav.classList.toggle("active");
+  }
+
+  // Close all other Slider Navigations that are not in use
+  document.querySelectorAll("[data-slider].active").forEach((slider) => {
+    if (slider == currentSliderNav) return;
+    slider.classList.remove("active");
+  });
+});
